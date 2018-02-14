38b69e741f664a31b110958a302c34de2cf9a892
zhijie.wang <wangzhijiebill@gmail.com>
2018-02-13 22:29:53 -0800

    implement AddEdge, AddNode

--- graph/graph.go
+++ graph/graph.go
@@ -2,21 +2,20 @@ package main
 
 // Graph data structure to hold value
 type Graph struct {
-	nodes []Node
-	edges []Edge
+	nodes   []*Node
+	edges   []*Edge
+	counter int
 }
 
 // Node data structure
 type Node struct {
-	id    int
 	label int
 	edges []*Edge
 }
 
 // Edge data structure
 type Edge struct {
-	id       int
-	lable    int
+	label    int
 	directed bool
 	fromNode *Node
 	toNode   *Node
@@ -25,7 +24,23 @@ type Edge struct {
 // NewGraph constructor
 func NewGraph() *Graph {
 	return &Graph{
-		nodes: make([]Node, 0),
-		edges: make([]Edge, 0),
+		nodes:   make([]*Node, 0),
+		edges:   make([]*Edge, 0),
+		counter: 0,
 	}
+}
+
+// AddNode add a constructed node into the graph
+func (g *Graph) AddNode(n *Node) {
+	g.nodes = append(g.nodes, n)
+	g.counter++
+}
+
+// AddEdge create an edge given the from node and to node, and direction flag. If direction flag is set to true, then the edge will be Node 1 -> Node 2, else it will ignore direction
+func (g *Graph) AddEdge(from *Node, to *Node, directed bool) {
+	e := &Edge{label: 0, directed: directed, fromNode: from, toNode: to}
+	g.edges = append(g.edges, e)
+	from.edges = append(from.edges, e)
+	to.edges = append(to.edges, e)
+	g.counter++
 }
--- graph/main.go
+++ graph/main.go
@@ -1,1 +1,10 @@
 package main
+
+func main() {
+	g := NewGraph()
+	n := Node{}
+	g.AddNode(&n)
+	n1 := Node{}
+	g.AddNode(&n1)
+	g.AddEdge(&n, &n1, false)
+}
