a645c21cc9874b838dc3714d557de23d0cb5f366
bill.wang <wangzhijiebill@gmail.com>
2018-02-21 23:06:48 -0800

    add java template support

---
+++ gforg/java_template/main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+import java.lang.*;
+import java.io.*;import java.io.InputStreamReader;
+class main
+ {
+	public static void main (String[] args)
+	 {
+	 //code
+      BufferedReader br = null;
+br = new BufferedReader(new InputStreamReader(System.in));
+     System.out.println("Hi");
+     System.out.println(args);
+     String input = br.readLine();
+     System.out.println(input);
+	 }
+}
