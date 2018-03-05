e2f53a5625ba33db0d12fd0f41b046f6c83398bc
ZhijieWang <wanghizhijiebill@gmail.com>
2018-03-04 16:54:29 -0800

    play with bit shifts

--- practice/simplesetbits/simplesetbits.go
+++ practice/simplesetbits/simplesetbits.go
@@ -1,18 +1,25 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 )
 
 func main() {
-	fmt.Println("Please enter some value\n")
-	reader := bufio.NewReader(os.Stdin)
-	result, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println("Something wrong %s", err)
-	}
-	fmt.Println(result)
-	fmt.Println("End of program \n")
+	//fmt.Println("Please enter some value\n")
+	//reader := bufio.NewReader(os.Stdin)
+	//result, err := reader.ReadString('\n')
+	//if err != nil {
+	//	fmt.Println("Something wrong %s", err)
+	//}
+	//fmt.Println(result)
+	//fmt.Println("End of program \n")
+	fmt.Println(setBit(4))
+}
+func setBit(a int) int {
+	fmt.Println(a >> 1)
+	b := a >> 1
+	fmt.Println(b)
+	fmt.Println(b >> 1)
+	return 0
+
 }
