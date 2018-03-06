d84fc1c243d5a1a9a4d9c33b728100d153e108c6
bill.wang <wangzhijiebill@gmail.com>
2018-03-06 14:46:20 -0800

    recursive nlogn call

--- practice/simplesetbits/simplesetbits.go
+++ practice/simplesetbits/simplesetbits.go
@@ -28,9 +28,46 @@ func main() {
 	//}
 	//fmt.Println(result)
 	//fmt.Println("End of program \n")
-	fmt.Println(setBit(3))
-	fmt.Println(setBit(6))
-	fmt.Println(setBit(7))
+	//	fmt.Println(setBit(3) == 4)
+	//fmt.Println(setBit(6) == 9)
+	//fmt.Println(setBit(7) == 12)
+	//fmt.Println(setBit(9) == 15)
+	fmt.Println(setBit(9) == setBit(8)+setBit(1)+1)
+	//	fmt.Println(setBitDP(1))
+	//	fmt.Println(setBitDP(3))
+	//	fmt.Println(setBitDP(4))
+	fmt.Println(setBitFast(6))
+}
+
+//func seconfLeftMostBit(a int, offset int) {
+//	a = a - (1 << offset)
+//}
+func setBitFast(a int) int {
+	switch a {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	case 2:
+		return 2
+	default:
+		m := uint(leftMostBit(a))
+		if a-(1<<m) == 0 {
+			fmt.Println("stack cakk %i, %i\n", a-1, 1)
+			return setBitFast(a-1) + 1
+		}
+		fmt.Printf("stack call %i, %i\n", 1<<m, a-(1<<m))
+		return setBitFast(1<<m) + setBit(a-(1<<m)) + a - (1 << m)
+	}
+	// get result for num bits
+	return 0
+}
+func leftMostBit(a int) int {
+	var highestBit int
+	for ; a > 1; a = a >> 1 {
+		highestBit++
+	}
+	return highestBit
 }
 func setBit(a int) int {
 	start := 1
