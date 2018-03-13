ee66912f4c9b7187df087c85c07540cfddf1ff55
bill.wang <wangzhijiebill@gmail.com>
2018-03-12 21:47:27 -0700

    improve runtime

--- gforg/3-11-2018/run.java
+++ gforg/3-11-2018/run.java
@@ -26,7 +26,6 @@
 //7
 //8 6 5
 //8
-
 import java.util.*;
 import java.io.IOException;
 import java.lang.*;
@@ -36,17 +35,137 @@ import java.util.Arrays;
 
 class run 
 {
+    static class Reader
+    {
+        final private int BUFFER_SIZE = 1 << 16;
+        private DataInputStream din;
+        private byte[] buffer;
+        private int bufferPointer, bytesRead;
+
+        public Reader()
+        {
+            din = new DataInputStream(System.in);
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public Reader(String file_name) throws IOException
+        {
+            din = new DataInputStream(new FileInputStream(file_name));
+            buffer = new byte[BUFFER_SIZE];
+            bufferPointer = bytesRead = 0;
+        }
+
+        public String readLine() throws IOException
+        {
+            byte[] buf = new byte[64]; // line length
+            int cnt = 0, c;
+            while ((c = read()) != -1)
+            {
+                if (c == '\n')
+                    break;
+                buf[cnt++] = (byte) c;
+            }
+            return new String(buf, 0, cnt);
+        }
+
+        public int nextInt() throws IOException
+        {
+            int ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do
+            {
+                ret = ret * 10 + c - '0';
+            }  while ((c = read()) >= '0' && c <= '9');
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public long nextLong() throws IOException
+        {
+            long ret = 0;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        public double nextDouble() throws IOException
+        {
+            double ret = 0, div = 1;
+            byte c = read();
+            while (c <= ' ')
+                c = read();
+            boolean neg = (c == '-');
+            if (neg)
+                c = read();
+
+            do {
+                ret = ret * 10 + c - '0';
+            }
+            while ((c = read()) >= '0' && c <= '9');
+
+            if (c == '.')
+            {
+                while ((c = read()) >= '0' && c <= '9')
+                {
+                    ret += (c - '0') / (div *= 10);
+                }
+            }
+
+            if (neg)
+                return -ret;
+            return ret;
+        }
+
+        private void fillBuffer() throws IOException
+        {
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+            if (bytesRead == -1)
+                buffer[0] = -1;
+        }
+
+        private byte read() throws IOException
+        {
+            if (bufferPointer == bytesRead)
+                fillBuffer();
+            return buffer[bufferPointer++];
+        }
+
+        public void close() throws IOException
+        {
+            if (din == null)
+                return;
+            din.close();
+        }
+    }
     public static boolean debug = true;
     public static void println(Object o){
         if (debug){
             System.out.println(o.toString());
         }
     }
-    public static void main (String[] args)
+    public static void main (String[] args) throws IOException
     {
         //code
         int numCases;
-        Scanner scanner  = new Scanner(System.in);
+        Reader scanner  = new Reader();
         numCases = scanner.nextInt();
         while (numCases >0){
             numCases--;
@@ -55,17 +174,39 @@ class run
             for (int i = 0; i < numElements; i++){
                 elements[i] = scanner.nextInt();
             }
-            String star = ""+elements[numElements-1];
-            int result = checkSuperStar(elements, numElements-1);
-            int lastIndex = numElements;
-            while(lastIndex != result){
-                lastIndex= result;
-                 star = elements[result] + " " + star;
-                result = checkSuperStar(elements, lastIndex);
-               // println(result);
+            Stack<Integer> star = new Stack<Integer>();
+            // adding the right most one to the star stack
+            int current = Integer.MIN_VALUE;
+            // star.push(current);
+            int index = numElements-1;
+            int superStar = -1;
+            while(index>=0){
+                if (elements[index]> current){
+                    star.push(elements[index]);
+                    current = elements[index];
+                    superStar = current;
+                }else if (elements[index]==current){
+                    superStar = -1;
+                }
+                index--;
             }
-            System.out.println(star);
-            System.out.println(elements[result]);
+            String output = "";
+            
+            Integer[] result = new Integer[star.size()];
+            for(int i = 0; i < result.length; ++i){
+                result[i] = star.pop();
+            }
+            //     output = output + " " + star.pop();
+            // }
+            for (Integer s: result){
+                System.out.print(s);
+                System.out.print(" ");
+            }
+            System.out.println();
+            // System.out.println(output.substring(1)); 
+            System.out.println(superStar);
+            //System.out.println(star);
+            //System.out.println(elements[result]);
         }
         scanner.close();
     }
